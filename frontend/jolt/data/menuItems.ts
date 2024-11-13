56eb7abf2408af0037bc3413a4d0f9a22385eb1d
Karel <r0892111@ucll.be>
2024-11-13 23:35:12 +0200

    added dynamic menu pages

---
+++ frontend/jolt/data/menuItems.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from '../models/MenuItem';
+
+export const menuItems: MenuItem[] = [
+  { id: 1, name: 'Spaghetti Carbonara', price: '$12', restaurantId: 1 },
+  { id: 2, name: 'Fettuccine Alfredo', price: '$15', restaurantId: 1 },
+  { id: 3, name: 'Cheeseburger', price: '$10', restaurantId: 2 },
+  { id: 4, name: 'Fries', price: '$5', restaurantId: 2 },
+];
---
+++ frontend/jolt/models/MenuItem.ts
@@ -0,0 +1,7 @@
+export interface MenuItem {
+    id: number;
+    name: string;
+    price: string;
+    restaurantId: number;  // Link each menu item to a restaurant ID
+  }
+  
